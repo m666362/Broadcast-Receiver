5fec2e6593baa266cbabb07f4f7b87392bceb5b3
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-25 14:08:48 +0600

    full app done by button and headphone

--- app/src/main/java/org/richit/broadcastreceiver/MainActivity.java
+++ app/src/main/java/org/richit/broadcastreceiver/MainActivity.java
@@ -3,20 +3,43 @@ package org.richit.broadcastreceiver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyBroadcastReceiver myBroadcastReceiver;
+    IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        intentFilter = new IntentFilter( Intent.ACTION_HEADSET_PLUG );
+    }
+
+//    public void broadcastIntent(View view) {
+//        Intent intent = new Intent();
+//        intent.setAction("CUSTOM_INTENT");
+//        sendBroadcast(intent);
+//    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        registerReceiver( myBroadcastReceiver, intentFilter );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver( myBroadcastReceiver );
     }
 
     public void broadcastIntent(View view) {
-        Intent intent = new Intent();
-        intent.setAction("CUSTOM_INTENT");
-        sendBroadcast(intent);
+        registerReceiver( myBroadcastReceiver, intentFilter );
     }
 }
--- app/src/main/java/org/richit/broadcastreceiver/MyBroadcastReceiver.java
+++ app/src/main/java/org/richit/broadcastreceiver/MyBroadcastReceiver.java
@@ -3,15 +3,38 @@ package org.richit.broadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
+    String TAG = this.getClass().getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        ToneGenerator toneGenerator = new ToneGenerator( AudioManager.STREAM_MUSIC, 1000 );
+        if(intent.getAction().equals( Intent.ACTION_HEADSET_PLUG )){
 
-        Toast.makeText( context, "Activated", Toast.LENGTH_SHORT ).show();
-
+            int state = intent.getIntExtra( "state", -1 );
+            switch (state){
+                case 0:
+                    Log.d(TAG, "Headphone Unplugged");
+                    Toast.makeText( context, "Headphone Unplugged", Toast.LENGTH_SHORT).show();
+                    toneGenerator.startTone( ToneGenerator.TONE_CDMA_ABBR_ALERT, 3000 );
+                    break;
+                case 1:
+                    Log.d(TAG, "Headphone Plugged in");
+                    Toast.makeText( context, "Headphone Plugged in", Toast.LENGTH_SHORT).show();
+                    toneGenerator.startTone( ToneGenerator.TONE_CDMA_ABBR_ALERT, 3000 );
+                    break;
+                default:
+                    Log.d(TAG, "Default state");
+                    Toast.makeText( context, "Default state", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
     }
 
 }
